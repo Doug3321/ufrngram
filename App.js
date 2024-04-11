48be3a1200334b2474c2f7515f5397ae3feb2258
douglas gabriel <douglasgabriel30@gmail.com>
2024-04-11 16:17:53 -0300

    Tela de Login

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import LoginScreen from "./screens/LoginScreen";
 import HomeScreen from "./screens/HomeScreen";
 import CharListScreen from "./screens/ChatListScreen";
 import ChatScreen from "./screens/ChatScreen";
@@ -12,6 +13,7 @@ export default function App(){
   return(
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="LoginScreen" component={LoginScreen} options={{headerShown: false}}/>
         <Stack.Screen name="HomeScreen" component={HomeScreen} options={{headerShown: false}}/>
         <Stack.Screen name="ChatListScreen" component={CharListScreen} options={{title: 'Chat List'}}/>
         <Stack.Screen name="ChatScreen" component={ChatScreen} options={{title: 'Chat'}}/>
